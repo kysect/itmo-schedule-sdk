0d0967ebc04686a2c8b0ca68f1168149770354e6
Fredi Kats <fredikats@outlook.com>
2019-04-15 00:41:33 +0300

    Add method for requesting group's lesson list

---
+++ DeItmoApiWrapper/IScheduleApi.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using DeItmoApiWrapper.Models;
+using Refit;
+
+namespace DeItmoApiWrapper
+{
+    public interface IScheduleApi
+    {
+        [Get("/schedule_lesson_room")]
+        Task<List<RoomModel>> GetLessonRoomList();
+
+        [Get("/schedule_lesson_group/{group}")]
+        Task<GroupScheduleModel> GetLessonGroupSchedule(string group);
+    }
+}
