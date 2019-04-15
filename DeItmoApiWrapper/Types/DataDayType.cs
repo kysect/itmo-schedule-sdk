29f36e81fc559b501de5963beac3afa92e8c0089
Alexey I.m2strng4dtwrld <inredikawb@gmail.com>
2019-04-15 09:11:08 +0300

    Change some fields in GroupScheduleItemModel

---
+++ DeItmoApiWrapper/Types/DataDayType.cs
@@ -0,0 +1,12 @@
+﻿namespace DeItmoApiWrapper.Types
+{
+    public enum DataDayType
+    {
+        Monday = 0,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday
+    }
+}
---
+++ DeItmoApiWrapper/Types/DataWeekType.cs
@@ -0,0 +1,9 @@
+﻿namespace DeItmoApiWrapper.Types
+{
+    public enum DataWeekType
+    {
+        Both = 0,
+        Even = 1,
+        Odd = 2
+    }
+}
