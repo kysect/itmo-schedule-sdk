b021e55bb834e320f196fe69d1fc373d607f22aa
Fredi Kats <fredikats@outlook.com>
2019-04-17 00:21:51 +0300

    Add filter for day types

---
+++ ItmoScheduleApiWrapper/Filters/DayTypeScheduleFilter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using ItmoScheduleApiWrapper.Helpers;
+using ItmoScheduleApiWrapper.Models;
+using ItmoScheduleApiWrapper.Types;
+
+namespace ItmoScheduleApiWrapper.Filters
+{
+    public static class DayTypeScheduleFilter
+    {
+        public static List<ScheduleItemModel> Filter(IEnumerable<ScheduleItemModel> lessonList, DataWeekType weekType)
+        {
+            return lessonList
+                .Where(lesson => lesson.DataWeek.Compare(weekType))
+                .ToList();
+        }
+
+        public static List<ScheduleItemModel> Filter(IEnumerable<ScheduleItemModel> lessonList, DataDayType dayType)
+        {
+            return lessonList
+                .Where(lesson => lesson.DataDay == dayType)
+                .ToList();
+        }
+
+        public static List<ScheduleItemModel> Filter(IEnumerable<ScheduleItemModel> lessonList, DataWeekType weekType, DataDayType dayType)
+        {
+            return lessonList
+                .Where(lesson => lesson.DataWeek.Compare(weekType) && lesson.DataDay == dayType)
+                .ToList();
+        }
+    }
+}
