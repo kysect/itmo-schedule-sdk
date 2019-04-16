d30610c40113f60d3abf9cefa2a351e1fb756a1a
Fredi Kats <fredikats@outlook.com>
2019-04-17 00:03:58 +0300

    Add extensions for DataWeekType

---
+++ ItmoScheduleApiWrapper/Helpers/Extensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ItmoScheduleApiWrapper.Types;
+
+namespace ItmoScheduleApiWrapper.Helpers
+{
+    public static class Extensions
+    {
+        public static bool Compare(this DataWeekType first, DataWeekType second)
+        {
+            if (first == second)
+                return true;
+            if (first == DataWeekType.Both || second == DataWeekType.Both)
+                return true;
+
+            return false;
+        }
+
+        public static string ToRuString(this DataWeekType weekType)
+        {
+            switch (weekType)
+            {
+                case DataWeekType.Both:
+                    return string.Empty;
+                case DataWeekType.Even:
+                    return "четная";
+                case DataWeekType.Odd:
+                    return "нечетная";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(weekType), weekType, null);
+            }
+        }
+    }
+}
