7225415c07caaef0589ab70ee89416a5f4235fc0
Fredi Kats <fredikats@outlook.com>
2019-04-18 11:13:43 +0300

    Add method for filter today schedule

---
+++ ItmoScheduleApiWrapper/Helpers/DayScheduleRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ItmoScheduleApiWrapper.Filters;
+using ItmoScheduleApiWrapper.Models;
+using ItmoScheduleApiWrapper.Types;
+
+namespace ItmoScheduleApiWrapper.Helpers
+{
+    public static class DayScheduleRepository
+    {
+        public static List<ScheduleItemModel> GetTodaySchedule(this IEnumerable<ScheduleItemModel> scheduleItemList, DateConvertorService convertor)
+        {
+            (DataDayType dayType, DataWeekType weekType) dayType = convertor.Convert(DateTime.Today);
+            return DayTypeScheduleFilter.Filter(scheduleItemList, dayType.weekType, dayType.dayType);
+        }
+    }
+}
