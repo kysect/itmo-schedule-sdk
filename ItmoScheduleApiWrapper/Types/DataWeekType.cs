5cd2324afb04809304ed247eede279d4085ef2ff
Fredi Kats <fredikats@outlook.com>
2020-09-12 20:43:42 +0300

    feat: some code enhance

--- ItmoScheduleApiWrapper/Types/DataWeekType.cs
+++ ItmoScheduleApiWrapper/Types/DataWeekType.cs
@@ -1,9 +1,39 @@
-﻿namespace ItmoScheduleApiWrapper.Types
+﻿using System;
+
+namespace ItmoScheduleApiWrapper.Types
 {
     public enum DataWeekType
     {
         Both = 0,
         Even = 1,
         Odd = 2
+    }
+
+    public static class DataWeekTypeExtensions
+    {
+        public static bool Compare(this DataWeekType first, DataWeekType second)
+        {
+            if (first == second)
+                return true;
+            if (first == DataWeekType.Both || second == DataWeekType.Both)
+                return true;
+
+            return false;
+        }
+
+        public static string ToRuString(this DataWeekType weekType)
+        {
+            switch (weekType)
+            {
+                case DataWeekType.Both:
+                    return "все";
+                case DataWeekType.Even:
+                    return "четная";
+                case DataWeekType.Odd:
+                    return "нечетная";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(weekType), weekType, null);
+            }
+        }
     }
 }
