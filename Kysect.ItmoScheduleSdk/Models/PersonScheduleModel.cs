e8828e622b51de38c608e9a8ca6c2ccb1b716f68
Fredi Kats <inredikawb@gmail.com>
2021-03-21 02:56:27 +0300

    feat: update Refit version, remove dependencies to NewtonJson

--- Kysect.ItmoScheduleSdk/Models/PersonScheduleModel.cs
+++ Kysect.ItmoScheduleSdk/Models/PersonScheduleModel.cs
@@ -1,5 +1,5 @@
-﻿using System.Collections.Generic;
-using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
 
 namespace Kysect.ItmoScheduleSdk.Models
 {
@@ -7,15 +7,15 @@ namespace Kysect.ItmoScheduleSdk.Models
     {
         public string Query { get; set; }
 
-        [JsonProperty(PropertyName = "Label")] public string PersonName { get; set; }
+        [JsonPropertyName("Label")] public string PersonName { get; set; }
 
-        [JsonProperty(PropertyName = "type_title")]
+        [JsonPropertyName("type_title")]
         public string TypeTitle { get; set; }
 
         public int Pid { get; set; }
         public string Today { get; set; }
 
-        [JsonProperty(PropertyName = "current_week")]
+        [JsonPropertyName("current_week")]
         public string CurrentWeek { get; set; }
 
         public List<ScheduleItemModel> Schedule { get; set; }
