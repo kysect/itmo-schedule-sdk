0cfc73954519d7c5846036d63e34da3d0bcac1cf
annchous <annchooousss@gmail.com>
2021-09-11 15:11:32 +0300

    feat: DataDay rusification

--- Kysect.ItmoScheduleSdk/Types/DataDayType.cs
+++ Kysect.ItmoScheduleSdk/Types/DataDayType.cs
@@ -23,5 +23,26 @@ namespace Kysect.ItmoScheduleSdk.Types
             int itmoDayType = dayType == 0 ? 6 : dayType - 1;
             return (DataDayType)itmoDayType;
         }
+        
+        public static string ToRuString(this DataDayType? dayType)
+        {
+            switch (dayType)
+            {
+                case DataDayType.Monday:
+                    return "Понедельник";
+                case DataDayType.Tuesday:
+                    return "Вторник";
+                case DataDayType.Wednesday:
+                    return "Среда";
+                case DataDayType.Thursday:
+                    return "Четверг";
+                case DataDayType.Friday:
+                    return "Пятница";
+                case DataDayType.Saturday:
+                    return "Суббота";
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(dayType), dayType, null);
+            }
+        }
     }
 }
